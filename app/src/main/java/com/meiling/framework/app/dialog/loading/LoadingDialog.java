515003ace0948fde02b774b3b98183c802173fc9
huangzhou <marisareimu@126.com>
2021-03-23 15:15:22 +0800

    基类构建 --- 4 --- Dialog加载【DataBinding】

---
+++ app/src/main/java/com/meiling/framework/app/dialog/loading/LoadingDialog.java
@@ -0,0 +1,73 @@
+package com.meiling.framework.app.dialog.loading;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+
+import com.meiling.framework.R;
+import com.meiling.framework.databinding.DialogLoadingBinding;
+import com.meiling.framework.dialog.base.BaseFragmentDialog;
+import com.meiling.framework.dialog.base.DialogConfig;
+import com.meiling.framework.dialog.callback.IDialogDismissCallback;
+import com.meiling.framework.dialog.callback.IDialogShowCallback;
+import com.meiling.framework.utils.datahandle.UnitExchangeUtil;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+
+public class LoadingDialog extends BaseFragmentDialog<DialogLoadingBinding> {
+    private String hintMsg;
+    private boolean isCancel = true;
+
+    @Override
+    public void setDialogConfig(@NonNull Context context, IDialogShowCallback iDialogShowCallback, IDialogDismissCallback iDialogDismissCallback) {
+        this.config = new DialogConfig.Builder().
+                setDialogStyle(R.style.Dialog_NoTitle_AlphaIn2).
+                setDialogShowPosition(/*Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL*/Gravity.CENTER).
+                setContentViewLayout(R.layout.dialog_loading).
+                setCancelable(isCancel).
+                setCancelOutside(isCancel).
+                setCancelForBackKey(isCancel).
+                setShowCallback(iDialogShowCallback).
+                setDismissCallback(iDialogDismissCallback).
+                setDialogWidth(UnitExchangeUtil.dip2px(context, 125)).
+                setDialogHeight(UnitExchangeUtil.dip2px(context, 125)).
+                build();
+    }
+
+    @Override
+    public void initContentView(View view) {
+        layoutDialogBinding.pbLoading.setVisibility(View.VISIBLE);
+        if (TextUtils.isEmpty(hintMsg)) {
+            layoutDialogBinding.tvLoadingHint.setVisibility(View.GONE);
+        } else {
+            layoutDialogBinding.tvLoadingHint.setVisibility(View.VISIBLE);
+            layoutDialogBinding.tvLoadingHint.setText(hintMsg);
+        }
+    }
+
+    public void setHintMsg(String hintMsg) {
+        this.hintMsg = hintMsg;
+    }
+
+    public void modifyHintMsg(String hintMsg) {
+        if (layoutDialogBinding.tvLoadingHint != null) {
+            layoutDialogBinding.tvLoadingHint.setText(hintMsg);
+        }
+    }
+
+    public void setCancel(boolean cancel) {
+        isCancel = cancel;
+    }
+
+    @Override
+    public void onDismiss(DialogInterface dialog) {
+        super.onDismiss(dialog);
+    }
+
+    public void show(FragmentManager manager, String tag, int delayTime) {
+        super.show(manager, tag);
+    }
+}
--- app/src/main/java/com/meiling/framework/app/fragment/DataBindFragment.java
+++ app/src/main/java/com/meiling/framework/app/fragment/DataBindFragment.java
@@ -1,9 +1,14 @@
 package com.meiling.framework.app.fragment;
 
+import android.text.TextUtils;
+import android.view.View;
+
 import com.meiling.framework.R;
+import com.meiling.framework.app.dialog.loading.LoadingDialog;
 import com.meiling.framework.app.viewmodel.data.Data;
 import com.meiling.framework.base.BaseFragment;
 import com.meiling.framework.databinding.FragmentDataBindBinding;
+import com.meiling.framework.dialog.callback.IDialogDismissCallback;
 
 public class DataBindFragment extends BaseFragment<FragmentDataBindBinding> {
 
@@ -22,10 +27,28 @@ public class DataBindFragment extends BaseFragment<FragmentDataBindBinding> {
         Data data = new Data();
         data.setName("Fragment");
         layoutFragmentBinding.setNameEntity(data);
+        layoutFragmentBinding.setClickPresenter(new FragmentClickPresenter());
     }
 
     @Override
     public void lazyLoadCallback() {
 
+    }
+    private LoadingDialog loadingDialog;
+    public class FragmentClickPresenter{
+        public void showDialog(View view){
+            if(loadingDialog==null){
+                loadingDialog = new LoadingDialog();
+                loadingDialog.setDialogConfig(getContext(), null, new IDialogDismissCallback() {
+                    @Override
+                    public void afterDialogDismiss() {
+                        if (loadingDialog != null) {
+                            loadingDialog = null;
+                        }
+                    }
+                });
+                loadingDialog.show(getFragmentManager(), "loading", 2000);
+            }
+        }
     }
 }
